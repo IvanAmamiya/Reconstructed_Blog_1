716e22c4bfb2dbe8a57490d39dc90f31e8f3643f
IvanAmamiya <106081022+IvanAmamiya@users.noreply.github.com>
2022-09-01 10:52:20 +0800

    0.1.1.1.1

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,19 @@
     
 
     <el class="app">
-      <RouterView/> 
+      
+      <el-menu
+    :default-active="activeIndex"
+    class="el-menu-demo"
+    mode="horizontal"
+    @select="handleSelect"
+  >
+    <el-menu-item index="1">Rokossovskaya的个人Blog</el-menu-item>
+
+    <el-menu-item index="3" >文章</el-menu-item>
+    <el-menu-item index="4">提问箱</el-menu-item>
+  </el-menu>
+  <RouterView/> 
    </el>
 
 </template>
---
+++ src/Home.vue
@@ -0,0 +1,6 @@
+<template>
+      <RouterView/> 
+      <h1>主页模块</h1>
+      
+</template>
+
